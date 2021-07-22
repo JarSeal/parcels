a1f42397cb0aa66208ac9de6e8969292a91a8782
JarSeal <kai.forsman@gmail.com>
2021-07-22 10:49:26 +0800

    Fix player and box starting positions (for now).

--- src/js/data/userPlayerData.js
+++ src/js/data/userPlayerData.js
@@ -1,7 +1,7 @@
 const userPlayerData = {
     id: 'some-unique-player-id',
     name: 'Keijo',
-    position: [12, 2, 12],
+    position: [15, 2, 4],
     direction: 0,
     speed: 2.8,
     jump: 4.8,
