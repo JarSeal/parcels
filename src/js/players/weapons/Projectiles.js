9ff2f6268a5dbc52b9f61de33965a92967ae5982
JarSeal <kai.forsman@gmail.com>
2021-07-26 00:57:52 +0800

    First particles.

--- src/js/players/weapons/Projectiles.js
+++ src/js/players/weapons/Projectiles.js
@@ -1,12 +1,13 @@
+import * as THREE from 'three';
+
 class Projectiles {
     constructor(sceneState) {
         this.sceneState = sceneState;
+        this.material = this._createParticleShader();
         this._initProjectiles();
     }
 
     _initProjectiles() {
-        console.log('TADAA!');
-
         // Init points here:
         // Create a large number of points (float32 attributes as vertices for the BufferGeometry)
         // (let's say about 30 x 100 = 3000, this means 30 simultaneous projectiles)
@@ -16,6 +17,49 @@ class Projectiles {
         // Add a SPEED uniform (float) for each projectile.
         // Add a HIT uniform (boolean) for each projectile.
         // Every uniform is an array where each index represents a projectile.
+
+        const startPosX = 15;
+        const startPosZ = 4;
+
+        const projCount = 2;
+        const particlesPerProjectile = 5;
+        const positions = new Float32Array(projCount * particlesPerProjectile * 3);
+        let i = 0;
+        for(let p1=0; p1<projCount; p1++) {
+            for(let p2=0; p2<particlesPerProjectile; p2++) {
+                positions[i] = p2 + startPosX;
+                positions[i+1] = 0.1;
+                positions[i+2] = p1 + startPosZ;
+                i += 3;
+            }
+        }
+        const projGeo = new THREE.BufferGeometry();
+        projGeo.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+        const particles = new THREE.Points(projGeo, this.material);
+        this.sceneState.scenes[this.sceneState.curScene].add(particles);
+    }
+
+    _createParticleShader() {
+        return new THREE.ShaderMaterial({
+            uniforms: {
+                color: { value: new THREE.Color(0xffffff) },
+            },
+            vertexShader: `
+                void main() {
+                    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+                    gl_PointSize = 10.0;
+                    gl_Position = projectionMatrix * mvPosition;
+                }
+            `,
+            fragmentShader: `
+                uniform vec3 color;
+                void main() {
+                    if(length(gl_PointCoord - vec2(0.5, 0.5)) > 0.475) discard;
+                    gl_FragColor = vec4(color, 1.0);
+                }
+            `,
+        });
+
     }
 }
 
