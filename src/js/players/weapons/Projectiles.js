b6657788a477af738140822aa0046123939c41b0
JarSeal <kai.forsman@gmail.com>
2021-07-25 12:14:18 +0300

    Pseudo code for the projectile particles.

--- src/js/players/weapons/Projectiles.js
+++ src/js/players/weapons/Projectiles.js
@@ -6,6 +6,16 @@ class Projectiles {
 
     _initProjectiles() {
         console.log('TADAA!');
+
+        // Init points here:
+        // Create a large number of points (float32 attributes as vertices for the BufferGeometry)
+        // (let's say about 30 x 100 = 3000, this means 30 simultaneous projectiles)
+        // Add a COLOR uniform (THREE.Color) for each projectile.
+        // Add a FROM uniform (THREE.Vector3) for each projectile.
+        // Add a TO uniform (THREE.Vector3) for each projectile.
+        // Add a SPEED uniform (float) for each projectile.
+        // Add a HIT uniform (boolean) for each projectile.
+        // Every uniform is an array where each index represents a projectile.
     }
 }
 
