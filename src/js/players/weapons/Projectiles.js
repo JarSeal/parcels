36d47d64b06bccd498b26b0d33fce53daa93c41a
JarSeal <kai.forsman@gmail.com>
2021-08-07 13:50:26 +0800

    New projectile graphics and functionality.

--- src/js/players/weapons/Projectiles.js
+++ src/js/players/weapons/Projectiles.js
@@ -12,7 +12,7 @@ class Projectiles {
     _initProjectiles() {
         // Init points here:
         // Create a large number of points (float32 attributes as vertices for the BufferGeometry)
-        // (let's say about 30 x 100 = 3000, this means 30 simultaneous projectiles)
+        // (let's say about 30 x 100 = 3000, this means 100 simultaneous projectiles)
         // Add a COLOR uniform (THREE.Color) for each projectile.
         // Add a FROM uniform (THREE.Vector3) for each projectile.
         // Add a TO uniform (THREE.Vector3) for each projectile.
@@ -22,9 +22,12 @@ class Projectiles {
 
         const projCount = this.maxProjectiles;
         const particlesPerProjectile = 30;
+        const trailParticlesStart = 25;
+        const trailParticlesStop = 30;
         const positions = new Float32Array(projCount * particlesPerProjectile * 3);
         const delays = new Float32Array(projCount * particlesPerProjectile);
         const projIndexes = new Float32Array(projCount * particlesPerProjectile);
+        const trails = new Float32Array(projCount * particlesPerProjectile);
         let i = 0;
         for(let p1=0; p1<projCount; p1++) {
             for(let p2=0; p2<particlesPerProjectile; p2++) {
@@ -34,12 +37,18 @@ class Projectiles {
                 positions[i+1] = 0;
                 positions[i+2] = 0;
                 i += 3;
+                if(p2 >= trailParticlesStart-1 && p2 <= trailParticlesStop-1) {
+                    trails[p1*particlesPerProjectile+p2] = Math.random();
+                } else {
+                    trails[p1*particlesPerProjectile+p2] = 1.1;
+                }
             }
         }
         const projGeo = new THREE.BufferGeometry();
         projGeo.setAttribute('projindex', new THREE.BufferAttribute(projIndexes, 1));
         projGeo.setAttribute('position', new THREE.BufferAttribute(positions, 3)); // This is used for the initial unused state (hidden from view)
         projGeo.setAttribute('delay', new THREE.BufferAttribute(delays, 1));
+        projGeo.setAttribute('trails', new THREE.BufferAttribute(trails, 1));
         this.particles = new THREE.Points(projGeo, this.material);
         this.particles.frustumCulled = false;
         console.log(this.particles);
@@ -58,13 +67,13 @@ class Projectiles {
                 uColors: { value: [ new THREE.Color(0xffff00), new THREE.Color(0xffffff), new THREE.Color(0xff0000) ] },
                 uTime: { value: 0 },
                 uStartTimes: { value: [ performance.now(), performance.now(), performance.now() ] },
-                uSpeeds: { value: [ 0.2, 1, 2 ] },
+                uSpeeds: { value: [ 0.15, 1, 2 ] },
                 uDistances: { value: [ 8, 2, 2 ] },
                 uFroms: { value: [ new THREE.Vector3(12, 1, 4), new THREE.Vector3(12, 1, 6), new THREE.Vector3(12, 1, 8) ] },
                 uTos: { value: [ new THREE.Vector3(20, 1, 6), new THREE.Vector3(14, 1, 6), new THREE.Vector3(14, 1, 8) ] },
-                uPixelRatio: { value: pixelRatio },
+                uPixelRatio: { value: pixelRatio / 10 },
                 diffuseTexture: { value: new THREE.TextureLoader().load(                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-                    this.sceneState.settings.assetsUrl + '/sprites/orange_glow2_256x256.png'
+                    this.sceneState.settings.assetsUrl + '/sprites/white_glow_256x256.png'
                 )},
             },
             depthTest: true,
@@ -74,6 +83,7 @@ class Projectiles {
             vertexShader: `
                 attribute float delay;
                 attribute float projindex;
+                attribute float trails;
                 uniform float uStartTime;
                 uniform float uTime;
                 uniform vec3 uFroms[${this.maxProjectiles}];
@@ -85,6 +95,9 @@ class Projectiles {
                 uniform float uPixelRatio;
                 varying float vTimePhase;
                 varying vec3 vColor;
+                varying float vIsTrail;
+                varying float vTrailTime;
+                varying float vDelay;
 
                 void main() {
                     int intIndex = int(projindex);
@@ -96,10 +109,16 @@ class Projectiles {
                     vTimePhase = mod(timeElapsed, travelTime) / travelTime;
                     vec3 from = uFroms[intIndex];
                     vec3 to = uTos[intIndex];
-                    vec3 newPos = from + (to - from) * vTimePhase;
-                    vec4 mvPosition = modelViewMatrix * vec4(newPos, 1.0);
+                    vIsTrail = clamp(floor(vTimePhase / trails), 0.0, 1.0);
+                    float notTrail = 1.0 - vIsTrail;
+                    vTrailTime = trails;
+                    vDelay = delay;
+                    vec3 trailPos = (to - from) * (trails + 0.2 * vTimePhase) * vIsTrail;
+                    vec3 newPos = (to - from) * vTimePhase * notTrail;
+                    vec4 mvPosition = modelViewMatrix * vec4(from + newPos + trailPos, 1.0);
                     vec4 vertexPosition = projectionMatrix * mvPosition;
-                    gl_PointSize = (2000.0 * (uPixelRatio / 10.0)) / distance(vertexPosition, mvPosition);
+                    float size = vIsTrail * 5500.0 + notTrail * 4000.0 * (1.0 - delay / 2.0);
+                    gl_PointSize = (size * uPixelRatio) / distance(vertexPosition, mvPosition);
                     gl_Position = vertexPosition;
                 }
             `,
@@ -107,6 +126,9 @@ class Projectiles {
                 uniform sampler2D diffuseTexture;
                 varying float vTimePhase;
                 varying vec3 vColor;
+                varying float vIsTrail;
+                varying float vTrailTime;
+                varying float vDelay;
 
                 void main() {
                     // if(length(gl_PointCoord - vec2(0.5, 0.5)) > 0.475) discard;
@@ -116,7 +138,8 @@ class Projectiles {
                     //     1.0 - (vTimePhase / 200.0),
                     //     1.0 - (vTimePhase / 200.0)
                     // , 1.0);
-                    gl_FragColor = texture2D(diffuseTexture, gl_PointCoord) * vec4(vColor, 1.0);
+                    float alpha = 1.0 - (vTimePhase / clamp(vTrailTime + 0.7, 0.0, 1.0)) * vIsTrail;
+                    gl_FragColor = texture2D(diffuseTexture, gl_PointCoord) * vec4(vColor + (0.5 - vDelay), alpha);
                 }
             `,
         });
