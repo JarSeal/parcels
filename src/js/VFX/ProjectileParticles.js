c8c9798db752799fce7a923c1b8b5c022adc34dd
JarSeal <kai.forsman@gmail.com>
2021-08-16 14:32:18 +0800

    Uniform arrays to attributes.

--- src/js/VFX/ProjectileParticles.js
+++ src/js/VFX/ProjectileParticles.js
@@ -25,30 +25,42 @@ class Projectiles {
         const trailParticlesStart = this.trailParticlesStart;
         const trailParticlesStop = this.trailParticlesStop;
         const positions = new Float32Array(projCount * particlesPerProjectile * 3);
-        const delays = new Float32Array(projCount * particlesPerProjectile);
-        const projIndexes = new Float32Array(projCount * particlesPerProjectile);
-        const trails = new Float32Array(projCount * particlesPerProjectile);
+        const targets = new Float32Array(projCount * particlesPerProjectile * 3);
+        const timeSpeedDelays = new Float32Array(projCount * particlesPerProjectile * 3);
+        const colors = new Float32Array(projCount * particlesPerProjectile * 3);
+        const distanceTrails = new Float32Array(projCount * particlesPerProjectile * 3);
         let i = 0;
         for(let p1=0; p1<projCount; p1++) {
             for(let p2=0; p2<particlesPerProjectile; p2++) {
-                projIndexes[p1*particlesPerProjectile+p2] = p1;
-                delays[p1*particlesPerProjectile+p2] = p2 * 0.02;
                 positions[i] = 0;
-                positions[i+1] = 0;
+                positions[i+1] = 2000;
                 positions[i+2] = 0;
+                targets[i] = 0;
+                targets[i+1] = 2000;
+                targets[i+2] = 0;
+                timeSpeedDelays[i] = 0;
+                timeSpeedDelays[i+1] = 0;
+                timeSpeedDelays[i+2] = p2 * this.delayPerParticle;
+                const color = new THREE.Color(0xff0000);
+                colors[i] = color.r;
+                colors[i+1] = color.g;
+                colors[i+2] = color.b;
+                distanceTrails[i] = 0;
                 if(p2 >= trailParticlesStart-1 && p2 <= trailParticlesStop-1) {
-                    trails[p1*particlesPerProjectile+p2] = Math.random();
+                    distanceTrails[i+1] = Math.random();
                 } else {
-                    trails[p1*particlesPerProjectile+p2] = 1.1;
+                    distanceTrails[i+1] = 1.1;
                 }
+                distanceTrails[i+2] = 0;
                 i += 3;
             }
         }
         const projGeo = new THREE.BufferGeometry();
-        projGeo.setAttribute('projindex', new THREE.BufferAttribute(projIndexes, 1));
-        projGeo.setAttribute('position', new THREE.BufferAttribute(positions, 3)); // This is used for the initial unused state (hidden from view)
-        projGeo.setAttribute('delay', new THREE.BufferAttribute(delays, 1));
-        projGeo.setAttribute('trails', new THREE.BufferAttribute(trails, 1));
+        projGeo.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+        projGeo.setAttribute('target', new THREE.BufferAttribute(targets, 3));
+        projGeo.setAttribute('timeSpeedDelay', new THREE.BufferAttribute(timeSpeedDelays, 3));
+        projGeo.setAttribute('distanceTrail', new THREE.BufferAttribute(distanceTrails, 3));
+        projGeo.setAttribute('color', new THREE.BufferAttribute(colors, 3));
         this.particles = new THREE.Points(projGeo, this.material);
         this.particles.frustumCulled = false;
         this.sceneState.scenes[this.sceneState.curScene].add(this.particles);
@@ -64,12 +76,31 @@ class Projectiles {
 
     newProjectile(from, to, distance, weapon, intersect) {
         const index = this.nextProjIndex;
-        this.material.uniforms.uColors.value[index] = new THREE.Color(weapon.color);
-        this.material.uniforms.uFroms.value[index] = from;
-        this.material.uniforms.uTos.value[index] = to;
-        this.material.uniforms.uDistances.value[index] = distance;
-        this.material.uniforms.uSpeeds.value[index] = weapon.speed;
-        this.material.uniforms.uStartTimes.value[index] = performance.now();
+        const attributes = this.particles.geometry.attributes;
+        let i;
+        const start = index * this.particlesPerProjectile * 3;
+        const end = start + this.particlesPerProjectile * 3;
+        const startTime = performance.now();
+        const color = new THREE.Color(weapon.color);
+        for(i=start; i<end; i+=3) {
+            attributes.position.array[i] = from.x;
+            attributes.position.array[i+1] = from.y;
+            attributes.position.array[i+2] = from.z;
+            attributes.target.array[i] = to.x;
+            attributes.target.array[i+1] = to.y;
+            attributes.target.array[i+2] = to.z;
+            attributes.timeSpeedDelay.array[i] = startTime;
+            attributes.timeSpeedDelay.array[i+1] = weapon.speed;
+            attributes.distanceTrail.array[i] = distance;
+            attributes.color.array[i] = color.r;
+            attributes.color.array[i+1] = color.g;
+            attributes.color.array[i+2] = color.b;
+        }
+        attributes.position.needsUpdate = true;
+        attributes.target.needsUpdate = true;
+        attributes.timeSpeedDelay.needsUpdate = true;
+        attributes.distanceTrail.needsUpdate = true;
+        attributes.color.needsUpdate = true;
         this.nextProjIndex++;
         if(this.nextProjIndex > this.maxProjectiles-1) this.nextProjIndex = 0;
         if(intersect && intersect.object) {
@@ -78,11 +109,24 @@ class Projectiles {
             }, weapon.speed * (distance - 0.25) * 1000);
         }
         setTimeout(() => {
-            this.material.uniforms.uFroms.value[index] = new THREE.Vector3(0, 2000, 0);
-            this.material.uniforms.uTos.value[index] = new THREE.Vector3(0, 2000, 0);
-            this.material.uniforms.uDistances.value[index] = 0;
-            this.material.uniforms.uSpeeds.value[index] = 0;
-            this.material.uniforms.uStartTimes.value[index] = performance.now();
+            const start = index * this.particlesPerProjectile * 3;
+            const end = start + this.particlesPerProjectile * 3;
+            const startTime = performance.now();
+            for(i=start; i<end; i+=3) {
+                attributes.position.array[i] = 0;
+                attributes.position.array[i+1] = 2000;
+                attributes.position.array[i+2] = 0;
+                attributes.target.array[i] = 0;
+                attributes.target.array[i+1] = 2000;
+                attributes.target.array[i+2] = 0;
+                attributes.timeSpeedDelay.array[i] = startTime;
+                attributes.timeSpeedDelay.array[i+1] = 0;
+                attributes.distanceTrail.array[i] = 0;
+            }
+            attributes.position.needsUpdate = true;
+            attributes.target.needsUpdate = true;
+            attributes.timeSpeedDelay.needsUpdate = true;
+            attributes.distanceTrail.needsUpdate = true;
         }, weapon.speed * distance * 1000 + this.totalDelayPerProjectile);
     }
 
@@ -90,13 +134,7 @@ class Projectiles {
         let pixelRatio = window.devicePixelRatio;
         const material = new THREE.ShaderMaterial({
             uniforms: {
-                uColors: { value: this._initShaderPart('color') },
                 uTime: { value: 0 },
-                uStartTimes: { value: this._initShaderPart('startTime') },
-                uSpeeds: { value: this._initShaderPart('speed') },
-                uDistances: { value: this._initShaderPart('zeros') },
-                uFroms: { value: this._initShaderPart('position') },
-                uTos: { value: this._initShaderPart('position') },
                 scale: { type: 'f', value: window.innerHeight * pixelRatio / 2 },
                 diffuseTexture: { value: this.sceneState.levelAssets.fxTextures.projectileBall.texture },
             },
@@ -105,17 +143,12 @@ class Projectiles {
             transparent: true,
             blending: THREE.AdditiveBlending,
             vertexShader: `
-                attribute float delay;
-                attribute float projindex;
-                attribute float trails;
+                attribute vec3 target;
+                attribute vec3 timeSpeedDelay;
+                attribute vec3 distanceTrail;
+                attribute vec3 color;
                 uniform float uStartTime;
                 uniform float uTime;
-                uniform vec3 uFroms[${this.maxProjectiles}];
-                uniform vec3 uTos[${this.maxProjectiles}];
-                uniform vec3 uColors[${this.maxProjectiles}];
-                uniform float uStartTimes[${this.maxProjectiles}];
-                uniform float uSpeeds[${this.maxProjectiles}];
-                uniform float uDistances[${this.maxProjectiles}];
                 uniform float scale;
                 varying float vTimePhase;
                 varying vec3 vColor;
@@ -124,25 +157,25 @@ class Projectiles {
                 varying float vDelay;
 
                 void main() {
-                    int intIndex = int(projindex);
-                    vColor = uColors[intIndex];
-                    float speed = uSpeeds[intIndex];
-                    float startTime = uStartTimes[intIndex] + delay * 1000.0 * speed;
-                    float travelTime = speed * uDistances[intIndex] * 1000.0 + delay;
+                    vColor = color;
+                    vDelay = timeSpeedDelay[2];
+                    float speed = timeSpeedDelay[1];
+                    float startTime = timeSpeedDelay[0] + vDelay * 1000.0 * speed;
+                    float travelTime = speed * distanceTrail[0] * 1000.0 + vDelay;
                     float timeElapsed = uTime - startTime;
                     vTimePhase = clamp(mod(timeElapsed, travelTime) / travelTime + floor(timeElapsed / travelTime), 0.0, 1.0);
-                    vec3 from = uFroms[intIndex];
-                    vec3 to = uTos[intIndex];
+                    vec3 from = position;
+                    vec3 to = target;
+                    float trails = distanceTrail[1];
                     vIsTrail = clamp(floor(vTimePhase / trails), 0.0, 1.0);
                     float notTrail = 1.0 - vIsTrail;
                     vTrailTime = trails;
-                    vDelay = delay;
                     vec3 trailPos = (to - from) * (trails + 0.2 * vTimePhase) * vIsTrail;
                     vec3 newPos = (to - from) * vTimePhase * notTrail;
                     newPos.y = newPos.y * (1.0 - floor(vTimePhase)) + 2000.0 * floor(vTimePhase);
                     vec4 mvPosition = modelViewMatrix * vec4(from + newPos + trailPos, 1.0);
                     vec4 vertexPosition = projectionMatrix * mvPosition;
-                    float pSize = vIsTrail * 2.0 + notTrail * 0.58 * (1.0 - delay / 2.0);
+                    float pSize = vIsTrail * 2.0 + notTrail * 0.58 * (1.0 - vDelay / 2.0);
                     gl_PointSize = pSize * (scale / length(-mvPosition.xyz));
                     gl_Position = vertexPosition;
                 }
